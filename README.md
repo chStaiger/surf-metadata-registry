d11bb05ef946d822920128355eaccec01bf228b9
chStaiger <staigerchristine@gmail.com>
2025-10-17 11:09:20 +0200

    add functionality to manage ckan configs (#1)
    * add functionality to manage ckan configs
    
    * surfmet
    
    * update readme

--- README.md
+++ README.md
@@ -2,19 +2,47 @@
 
 # Dpendencies
 
+- Requires python 3.13 or higher because of the module `ckanapi`.
+- `ckanapi`
+
 # Installation
 
 ## Install a git branch
 
 ### pip
 
+```
+pip install git+https://github.com/chStaiger/surf-metadata-registry.git@<branch>
+surfmeta
+````
+
 ### uv
+
+```
+uv install git+https://github.com/chStaiger/surf-metadata-registry.git@<branch>
+uv run surfmeta
+```
 
 ## Checkout code and install locally
 
+```
+git clone https://github.com/chStaiger/surf-metadata-registry.git
+cd surf-metadata-registry
+```
+
 ### pip
 
+```
+pip install -e .
+surfmeta
+```
+
 ### uv
+
+```
+uv build
+uv run surfmeta
+```
 
 # Configuration
 
--- surfmeta/__main__.py
+++ surfmeta/__main__.py
@@ -2,24 +2,106 @@
 
 import argparse
 import sys
+from getpass import getpass
 from importlib.metadata import version
 
-MAIN_HELP_MESSAGE = f"""
+from surfmeta.ckan_conf import CKANConf, show_available
+
+MAIN_HELP_MESSAGE = """
 Create metadata for data on SURF infrastructure.
 
 Usage: surfmeta [subcommand] [options]
 
 Available subcommands:
+    ckan        Manage CKAN configurations
 
 Example usage:
-
+    surfmeta ckan list
+    surfmeta ckan switch myalias
+    surfmeta ckan init mytoken
+    surfmeta ckan remove demo
+    surfmeta ckan alias myalias https://demo.ckan.org
 """
+
+CKANCONFIG = CKANConf()
+
+def build_parser():
+    parser = argparse.ArgumentParser(
+        prog="surfmeta",
+        description=MAIN_HELP_MESSAGE,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    # Top-level subparser: `ckan`
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    ckan_parser = subparsers.add_parser("ckan", help="CKAN configuration commands")
+    ckan_subparsers = ckan_parser.add_subparsers(dest="ckan_command", required=True)
+
+    # `surfmeta ckan list`
+    parser_list = ckan_subparsers.add_parser("list", help="List all CKAN configurations")
+    parser_list.set_defaults(func=ckan_list)
+
+    # `surfmeta ckan switch url_or_alias`
+    parser_switch = ckan_subparsers.add_parser("switch", help="Switch active CKAN config")
+    parser_switch.add_argument("url_or_alias", help="URL or alias to switch to")
+    parser_switch.set_defaults(func=ckan_switch)
+
+    # `surfmeta ckan init password`
+    parser_init = ckan_subparsers.add_parser("init", help="Initialize CKAN config with token")
+    parser_init.add_argument("url_or_alias", help="CKAN URL or lias for the URL.")
+    parser_init.set_defaults(func=ckan_init)
+
+    # `surfmeta ckan remove url_or_alias`
+    parser_remove = ckan_subparsers.add_parser("remove", help="Remove CKAN config")
+    parser_remove.add_argument("url_or_alias", help="URL or alias to remove")
+    parser_remove.set_defaults(func=ckan_remove)
+
+    # `surfmeta ckan alias alias url`
+    parser_alias = ckan_subparsers.add_parser("alias", help="Set an alias for a CKAN URL")
+    parser_alias.add_argument("alias", help="Alias name")
+    parser_alias.add_argument("url", help="URL to associate with alias")
+    parser_alias.set_defaults(func=ckan_alias)
+
+    return parser
+
 
 def main():
     """CLI with different subcommands."""
-    subcommand = "--help" if len(sys.argv) < 2 else sys.argv.pop(1)
+    parser = build_parser()
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    args = parser.parse_args()
+    if hasattr(args, "func"):
+        args.func(args)
+    else:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
-    if subcommand in ["-h", "--help"]:
-        print(MAIN_HELP_MESSAGE)
-    elif subcommand in ["-v", "--version"]:
-        print(f"surfiamviz version {version('surfmeta')}")
+# CKAN CONFIG FUNCTIONS
+def ckan_list(args):
+    show_available(CKANCONFIG)
+
+def ckan_switch(args):
+    CKANCONFIG.set_ckan(args.url_or_alias)
+
+def ckan_alias(args):
+    CKANCONFIG.set_alias(args.alias, args.url)
+
+def ckan_init(args):
+    CKANCONFIG.set_ckan(args.url_or_alias)
+    url, entry = CKANCONFIG.get_entry()
+    if sys.stdin.isatty() or "ipykernel" in sys.modules:
+        token = getpass(f"Your CKAN token for {args.url_or_alias} : ")
+    else:
+        print(f"Your CKAN token for {args.url_or_alias} : ")
+        token = sys.stdin.readline().rstrip()
+    entry["token"] = token
+    CKANCONFIG.ckans[url] = entry  # Update the entry with the token
+
+    # Save the updated CKAN configuration
+    CKANCONFIG.save()
+
+def ckan_remove(args):
+    CKANCONFIG.delete_alias(args.url_or_alias)
